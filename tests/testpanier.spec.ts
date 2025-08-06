ea17ceef0bf3146632a20e629d500c4e8fe5b074
Gael NOKAM NOKAM <nokamnokamgael@gmail.com>
2025-08-06 15:11:00 +0200

    test panier

---
+++ tests/testpanier.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+import LoginPage from '../pages/LoginPage';
+import Panier from '../pages/PanierPag';
+test('test panier ', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  const loginPage:LoginPage= new LoginPage(page);
+  await loginPage.saisirUsername('standard_user');
+  await loginPage.saisirPassword('secret_sauce');
+  await loginPage.clicSurLogin();
+  await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html'); 
+  const panier = new Panier(page);
+  await panier.ajouterArticle();
+  await panier.ouvrirPanier();
+  await panier.supprimerArticle();
+  await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html'); 
+});
